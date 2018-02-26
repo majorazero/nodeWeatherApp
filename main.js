4edca2606e7f3849af93a665a8d460497f18f760
majorazero <daniel.cw.hsu@gmail.com>
2018-02-26 15:41:56 -0800

    accessed basic info from a weather app api and also found out that template literals are only usable in es6 with backticks

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const request = require('request');
+let request = require('request');
+
+//OpenWeatherMap API
+let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
+let city = "London";
+//note that to use template literals like ${} you need to use backticks ``
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//let url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=c033c7d88ddd656c159ed45f9a39923e";
 
 //this function requests a url and if there is an error it logs the errors and ends
 //OR if there is no error, logs the body.
-request(url, function(err,response,body){
+
+request(url, function(err,res,body){
   if(err){
-    console.log('error:',error);
+    console.log('error:',err);
   }
   else{
     console.log('body:',body);
   }
-
 });
-
-function hi(){
-  console.log(3);
-}
