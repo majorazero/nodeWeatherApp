11a72e449ae0ed74431b354a788a514d3c1ac469
majorazero <daniel.cw.hsu@gmail.com>
2018-02-26 16:01:30 -0800

    added a module that lets you specifiy which city you'd want to call for the temperature from the console

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let request = require('request');
 
+const argv = require('yargs').argv;
+
 //OpenWeatherMap API
 let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
-let city = "London";
+let city = argv.c || "London";
 //note that to use template literals like ${} you need to use backticks ``
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 //let url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=c033c7d88ddd656c159ed45f9a39923e";
@@ -20,3 +22,4 @@ request(url, function(err,res,body){
     console.log(message);
   }
 });
+//console.log(ci)
